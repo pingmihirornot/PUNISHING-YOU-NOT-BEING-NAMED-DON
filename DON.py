e981f15b7af0fe3fa967eaaa62d3e330b41fef09
pingmihirornot <pingmihir@gmail.com>
2026-05-11 17:53:23 +0530

    Create DON.py

---
+++ DON.py
@@ -0,0 +1,9 @@
+# here we start
+name = input("Enter your name: ")
+# HERE IS THE NAME
+if name == "DON" or name == "don":
+    print("YOU ARE THE SHIT")
+else:
+    print("I am sorry for your sorry ass")
+
+# wubba lubba dub dub baby
